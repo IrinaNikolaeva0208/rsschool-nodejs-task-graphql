f4cab441b4029e09a79f4b5588ea35bade547845
Dark_Soul <irina2002.nikolaeva@yandex.ru>
2023-01-31 00:03:46 +0300

    feat: limit gql query depth

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,9 +1,10 @@
 import { FastifyPluginAsyncJsonSchemaToTs } from '@fastify/type-provider-json-schema-to-ts';
 import { graphqlBodySchema } from './schema';
-import { graphql } from 'graphql';
+import { graphql, validate, parse } from 'graphql';
 import root from './rootQuery';
 import querySchema from './queryScema';
 import { FastifyInstance } from 'fastify';
+import depthLimit = require('graphql-depth-limit');
 let context: FastifyInstance;
 
 const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
@@ -18,6 +19,8 @@ const plugin: FastifyPluginAsyncJsonSchemaToTs = async (
     },
     async function (request, reply) {
       context = fastify;
+      const error = validate(querySchema, parse(String(request.body.query)), [depthLimit(6)]);
+      if(error.length) reply.send(error);
       return await graphql({
         schema: querySchema,
         source: String(request.body.query),
